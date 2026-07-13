bbdbfc2ed5d79770f18d567bf06dfd942112e128
Moinuddin Patel <moinuddinpatel846@gmail.com>
2026-07-13 14:19:44 +0530

    Refactor Mail.js to enable email sending

--- backend/configs/Mail.js
+++ backend/configs/Mail.js
@@ -1,6 +1,6 @@
-// import nodemailer from "nodemailer"
-// import dotenv from "dotenv"
-// dotenv.config()
+import nodemailer from "nodemailer"
+import dotenv from "dotenv"
+dotenv.config()
 // const transporter = nodemailer.createTransport({
 //   service: "Gmail",
 //   port: 465,
@@ -10,81 +10,33 @@
 //     pass: process.env.EMAIL_PASS,
 //   },
 // });
-
-
-// const sendMail=async (to,otp) => {
-//     transporter.sendMail({
-//         from:process.env.EMAIL,
-//         to:to,
-//         subject:"Reset Your Password",
-//         html:`<p>Your OTP for Password Reset is <b>${otp}</b>.
-//         It expires in 5 minutes.</p>`
-//     })
-// }
-
-
-// export default sendMail
-
-
-import nodemailer from "nodemailer";
-import dotenv from "dotenv";
-
-dotenv.config();
-
-
 const transporter = nodemailer.createTransport({
 
-  host: "smtp.gmail.com",
+host:"smtp.gmail.com",
 
-  port: 587,
+port:587,
 
-  secure: false,
+secure:false,
 
-  auth: {
-    user: process.env.EMAIL,
-    pass: process.env.EMAIL_PASS,
-  },
+connectionTimeout:10000,
 
-  tls:{
-    rejectUnauthorized:false
-  }
+auth:{
+ user:process.env.EMAIL,
+ pass:process.env.EMAIL_PASS
+}
 
 });
 
-
-const sendMail = async(to,otp)=>{
-
-  try{
-
-    const info = await transporter.sendMail({
-
-      from: process.env.EMAIL,
-
-      to: to,
-
-      subject:"Reset Your Password",
-
-      html:`
-        <h2>Mentora Password Reset</h2>
-        <h1>${otp}</h1>
-        <p>This OTP expires in 5 minutes.</p>
-      `
-
-    });
-
-
-    console.log("MAIL SENT:",info.messageId);
-
-
-  }catch(error){
-
-    console.log("MAIL ERROR:",error);
-
-    throw error;
-
-  }
-
+const sendMail=async (to,otp) => {
+    transporter.sendMail({
+        from:process.env.EMAIL,
+        to:to,
+        subject:"Reset Your Password",
+        html:`<p>Your OTP for Password Reset is <b>${otp}</b>.
+        It expires in 5 minutes.</p>`
+    })
 }
 
 
-export default sendMail;
+export default sendMail
+
