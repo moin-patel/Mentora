5c40d41c4ca707d293eef814a2ddbde96338fba3
Moinuddin Patel <moinuddinpatel846@gmail.com>
2026-07-29 22:07:21 +0530

    Fix typo in axiosInstance.js filename

--- frontend/utils/axiosInstacnce.js
+++ frontend/utils/axiosInstacnce.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { toast } from "react-toastify";
 
 const api = axios.create({
-  baseURL: "https://mentoraa-fkwz.onrender.com"",
+  baseURL: "https://mentoraa-fkwz.onrender.com",
   timeout: 15000,
 });
 
